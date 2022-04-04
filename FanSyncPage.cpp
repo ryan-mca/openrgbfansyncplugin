616a1d504127d0b62a27ada07aa172044d7f0a08
Shady Nawara <shady.nawara@outlook.com>
2022-04-04 02:30:11 -0500

    update fan sync page

--- FanSyncPage.cpp
+++ FanSyncPage.cpp
@@ -3,12 +3,16 @@
 FanSyncPage::FanSyncPage(QWidget *parent)
     : QWidget{parent}
 {
+    // main v layout
     QVBoxLayout *layout = new QVBoxLayout();
     this->setLayout(layout);
 
+    // first instruction lines
     QLabel *instructionsLine1 = new QLabel("Add a measurement from the dropdown below then format the measurement function as required.");
     QLabel *instructionsLine2 = new QLabel("Most Mathmatical expressions are supported, example: \"({{cputemp}} + {{gputemp}}) / 2\". \n See: http://www.partow.net/programming/exprtk/ for more info.");
 
-    layout->addWidget(instructionsLine1);
-    layout->addWidget(instructionsLine2);
+    layout->addWidget(instructionsLine1, 1);
+    layout->addWidget(instructionsLine2, 1);
+
+    // hardware measures
 }
